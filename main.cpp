78d013cc829f8fab485d74aa2ed418d4514fe453
Dmytro Radchuk <3rubasa@gmail.com>
2023-08-30 22:16:27 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "Hello, from pmbus1!\n";
+
+// + #include "LT_PMBusDetect.h"
+// + #include "LT_PMBusDevice.h"
+// + #include "LT_PMBusRail.h"
+// + #include "LT_PMBusSpeedTest.h"
+// + #include "LT_PMBus.h"
+// + #include "LT_SMBus.h"
+// + #include "LT_SMBusGroup.h"
+// + #include "LT_PMBusMath.h"
+// + #include "LT_SMBusBase.h"
+// + #include "LT_Exception.h"
+}
